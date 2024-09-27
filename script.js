f47e2ad3b6ba73eb297eb2980a03982492440f73
WebMohit20 <2003mohitkhemani@gmail.com>
2024-09-27 06:59:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 //your JS code here. If required.
-let btn=document.querySelect("#openModal");
+let btn=document.getElementById("#openModal");
+let modal = document.getElementById("myModal");
+let close = document.querySelector("span");
 btn.addEventListener("click",()=>{
-	
+	modal.style.display = "block;"
 })
+
+
+close.addEventListener("click",()=>{
+	modal.style.display = "none;"
+})
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
